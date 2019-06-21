8625d29952f72d45caa312ee1849fea2bbd47fe7
jaywiles <jay.wiles@gmail.com>
2019-06-20 19:46:51 -0500

    temporarily took out

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -15,103 +15,68 @@
 
 
 
-if (player1 == player2) {
-  console.log("It's a tie!")
-} else if (player1 = "rock" && player2 = "scissors") {
-  console.log("player1 wins!")
-} else if (player1 = "paper" && player2 = "rock") {
-  console.log("player1 wins!")
-} else if (player1 = "scissors" && player2 = "paper") {
-  console.log("player1 wins!")
-} else () {
-  console.log("player2 wins!")
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // !!!!!!! CODE BELOW WAS IN THE FILE WHEN I FORKED FROM ACA GITHUB!!!!!!!!
 
-// 'use strict';
+'use strict';
 
-// const assert = require('assert');
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 
-// function rockPaperScissors(hand1, hand2) {
+function rockPaperScissors(hand1, hand2) {
 
-//   // Write code here
+  // Write code here
+  // if (hand1 == hand2) {
+  //   console.log("It's a tie!")
+  // } else if (hand1 = "rock" && hand2 = "scissors") {
+  //   console.log("hand1 wins!")
+  // } else if (hand1 = "paper" && hand2 = "rock") {
+  //   console.log("hand1 wins!")
+  // } else if (hand1 = "scissors" && hand2 = "paper") {
+  //   console.log("hand1 wins!")
+  // } else () {
+  //   console.log("hand2 wins!")
+  // }
 
-// }
+}
 
-// function getPrompt() {
-//   rl.question('hand1: ', (answer1) => {
-//     rl.question('hand2: ', (answer2) => {
-//       console.log( rockPaperScissors(answer1, answer2) );
-//       getPrompt();
-//     });
-//   });
-// }
+function getPrompt() {
+  rl.question('hand1: ', (answer1) => {
+    rl.question('hand2: ', (answer2) => {
+      console.log( rockPaperScissors(answer1, answer2) );
+      getPrompt();
+    });
+  });
+}
 
-// // Tests
+// Tests
 
-// if (typeof describe === 'function') {
+if (typeof describe === 'function') {
 
-//   describe('#rockPaperScissors()', () => {
-//     it('should detect a tie', () => {
-//       assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-//       assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-//       assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-//     });
-//     it('should detect which hand won', () => {
-//       assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-//       assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-//       assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-//     });
-//     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-//       assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-//       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-//       assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
-//     });
-//   });
-// } else {
+  describe('#rockPaperScissors()', () => {
+    it('should detect a tie', () => {
+      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
+      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
+      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
+    });
+    it('should detect which hand won', () => {
+      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+    });
+    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
+      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
+      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
+      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+    });
+  });
+} else {
 
-//   getPrompt();
+  getPrompt();
 
-// }
+}
