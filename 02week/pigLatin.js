a82d63467c253988321c09cac2626204e8971ace
jaywiles <jay.wiles@gmail.com>
2019-07-09 16:55:06 -0500

    sorta almost there

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -99,10 +99,12 @@ function displayPigLatin() {
   // this performs pigLatin function on word put into translator
   pigLatin(formInput);
   // this displays result
-  document.write(pigLatin(formInput));
+  // document.write(pigLatin(formInput));
+  // (testing more functionality below)
+  document.getElementById('listOfItemsID').innerHTML = pigLatin(formInput);
 }
 
-
+// getElementById('listOfItemsID')
 // Tests
 
 if (typeof describe === 'function') {
@@ -139,32 +141,32 @@ if (typeof describe === 'function') {
 
 
 
-$(function() {
-  let $submitButton = $('#submitButton');
-  let $listOfItemsID = $('#listOfItemsID');
+// $(function() {
+//   let $submitButton = $('#submitButton');
+//   let $listOfItemsID = $('#listOfItemsID');
   
-  $submitButton.on('click',function(e) {
-      e.preventDefault();
-      // above might be more specific to jQuery -- this particular function keeps the item from instantly going away after click
-      let $userInput = $('#userInput').val();
-      // console.log($userInput);
-      // line above tests for Chrome inspect tool
-      // $listOfItemsID.html($userInput);
-      // line above makes input show up on page after we type it in and hit enter
-      $(`<li>${$userInput}</li>`).appendTo($listOfItemsID);
-      // below clears submission so it's ready for the next one
-      // console.log(#userInput);
-      $('#userInput').val('');
-      // adding a space between '' in val() will clear the field above - not having the space will return it to the original text
+//   $submitButton.on('click',function(e) {
+//       e.preventDefault();
+//       // above might be more specific to jQuery -- this particular function keeps the item from instantly going away after click
+//       let $userInput = $('#userInput').val();
+//       // console.log($userInput);
+//       // line above tests for Chrome inspect tool
+//       // $listOfItemsID.html($userInput);
+//       // line above makes input show up on page after we type it in and hit enter
+//       $(`<li>${$userInput}</li>`).appendTo($listOfItemsID);
+//       // below clears submission so it's ready for the next one
+//       // console.log(#userInput);
+//       $('#userInput').val('');
+//       // adding a space between '' in val() will clear the field above - not having the space will return it to the original text
 
-  });
+//   });
 
-  // "this" is a JS keyword - dark blue means it's a reserved word in JS
-  $listOfItemsID.on('click', 'li', function() {
-      let $this = $(this);
-      // console.log($this);
-      $this.remove();
-  });
+//   // "this" is a JS keyword - dark blue means it's a reserved word in JS
+//   $listOfItemsID.on('click', 'li', function() {
+//       let $this = $(this);
+//       // console.log($this);
+//       $this.remove();
+//   });
 
 
-});
+// });
