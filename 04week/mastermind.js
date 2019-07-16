5af9d12be3f4c76de570fea1d621f1ed04ce4a64
jaywiles <jay.wiles@gmail.com>
2019-07-15 21:53:23 -0500

    functional

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -21,23 +21,17 @@ function printBoard() {
 }
 
 function generateSolution() {
-  for (let i = 0; i < 4; i++) {
-    const randomIndex = getRandomInt(0, letters.length);
-    solution += letters[randomIndex];
-  }
+	for (let i = 0; solution.split('').length < 4; i++) {
+		const randomIndex = getRandomInt(0, letters.length);
+		if (!solution.split('').includes(letters[randomIndex]) ) {
+			solution += letters[randomIndex];
+		}
+	}
 }
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
-// function validInput(guess) {
-//   for (i = 0; i <= guessArray.length; i++) {
-//     if 
-//     letters.includes(guess[i])
-//   }
-//   return false;
-// }
 
 function validInput(guessArray) {
   for (let i = 0; i < guessArray.length; i++) {
@@ -48,16 +42,32 @@ function validInput(guessArray) {
   return true;
 }
 
+const guessesRemaining = () => {
+  console.log(`You have ${10 - board.length} guesses remaining.`);
+  return board.length < 10;
+};
+
 function generateHint(guessArray, solutionArray) {
 	let redPeg = 0;
 	let whitePeg = 0;
-	for (let i = 0; i < guessArray.length; i++) {
-		if (guessArray[i] === solutionArray[i]) {
+	for ( let i = 0; i < solutionArray.length; i++ ) {
+		if ( guessArray[i] === solutionArray[i] ) {
 			redPeg++;
-		} else if (guessArray[i].includes(solutionArray)) {
-			whitePeg++;
+			// remove the guess that has already been tested
+			solutionArray[i] = null;
 		}
 	}
+	// Loop through the updated array to find all correct letters
+	// without adding more than one white peg for multiple correct letters
+	for (let i = 0; i < solutionArray.length; i++) {
+		let targetIndex = solutionArray.indexOf(guessArray[i]);
+		if(targetIndex > -1){
+			whitePeg++;
+			// remove all guesses that match the current guess
+			solutionArray[targetIndex] = null;
+		}
+  }
+  guessesRemaining();
 	console.log(colors.red(redPeg) + '-' + whitePeg);
 	return redPeg + '-' + whitePeg;
 }
@@ -76,7 +86,7 @@ function mastermind(guess) {
 			console.log('You guessed it!');
 			return 'You guessed it!'; 
 		} else if (validInput(guessArray)) {
-			board.unshift(guess + " " + generateHint(guessArray, solutionArray))
+			board.unshift(guess + ": " + generateHint(guessArray, solutionArray))
 		} else {
 			console.log('You must enter a letter between "a" and "h".');
 		}
@@ -122,64 +132,3 @@ if (typeof describe === 'function') {
   generateSolution();
   getPrompt();
 }
-
-
-
-
-
-// function generateHint(guess) {
-//   // starting the count for each of these so they can be logged below
-//   let correctLetterLocations = 0;
-//   let correctLetters = 0;
-//   // this function counts the number of characters that are also in the solution, and counts the number that are in the right place
-//   guessArray.forEach((letter, index) => {
-//     const correspondingLetter = solutionArray[index];
-//     if (letter == correspondingLetter) {
-//       correctLetterLocations++;
-//     } else if (solutionArray.includes(letter)) {
-//       correctLetters++;
-//     }
-//   });
-//   console.log(`You have ${correctLetters} correct characters, but only ${correctLetterLocations} are in the right spot.`);
-//   return correctLetterLocations + ' ' + correctLetters;
-// }
-
-// compare each letter
-// if right character and right spot, red peg || if not, white peg
-
-
-
-
-
-// take guess, which is a string, and parse it into an array
-// function validInput(guessArray) {
-//   let isValid = true; // usually set to true at first and then false when checking - easier to check when false
-//   if (!guessArray.includes(letters)) {
-//   isValid = false
-//   }
-// }
-
-// validInput() - new function
-// comparing guessArray to solution
-// if we have for loop of i.. if guessArray.charAt[i] is equal to solution.charAt[i]
-// will also need .indexAr
-
-
-
-
-
-// const isValidInput = () => {
-//   if (guess.includes(letters)) {
-//     mastermind();
-//   } else {
-//     console.log('Please input a letter from "a" to "h".')
-//   }
-// }
-
-
-
-  // if (letters.join().includes(guess.join())) {
-  //   return false;
-  // }
-  // console.log('Is this working?')
-  // return true
