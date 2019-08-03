14cc38fa5a521372e3fe28d73a67eaec9b0c0f82
jaywiles <jay.wiles@gmail.com>
2019-08-03 12:14:28 -0500

    done

--- 07week/fetchAddressBook/addressBook.js
+++ 07week/fetchAddressBook/addressBook.js
@@ -9,21 +9,25 @@
 //         }).catch((err) => console.log(err));
 // }
 
-document.getElementById("openBook").addEventListener('click', openBook)
+document.getElementById('openBook').addEventListener('click', openBook)
 
 function openBook() {
-    console.log("is this working?")
+    // console.log("is this working?")
     fetch("https://randomuser.me/api/?results=5")
         .then((res) => res.json())
             .then((data) => {
                 let output = '<h2>Addresses</h2>'
                 data.results.forEach(function(user) {
-                    output +=
+                    output +=`
+                        <div>
+                            <img src="${user.picture.large}">
+                        </div>
                         <ul>
-                            <li>Name: $(user.name.first}</li>
-                            
+                            <li>Name: ${user.name.first}</li>                        
+    						<li><strong>Age:</strong> ${user.dob.age}</li>
+							<li><strong>Email:</strong> ${user.email}</li>
                         </ul>
-                    ;
+                    `;
                 })
                 document.getElementById("input").innerHTML = output;
             })
