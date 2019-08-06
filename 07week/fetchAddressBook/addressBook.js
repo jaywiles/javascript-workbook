85ff953711d5cc59810a7c3c2fc86f75c2c3ff85
jaywiles <jay.wiles@gmail.com>
2019-08-06 18:25:30 -0500

    added error handling

--- 07week/fetchAddressBook/addressBook.js
+++ 07week/fetchAddressBook/addressBook.js
@@ -1,19 +1,16 @@
-// document.getElementById("openBook").addEventListener('click', openBook)
-
-// function openBook() {
-//     fetch("https://randomuser.me/api/?results=5")
-//         .then(function(data) {
-//             return data.text();
-//         }).then(function(print) {
-//             document.getElementById("input").innerHTML = print;
-//         }).catch((err) => console.log(err));
-// }
-
 document.getElementById('openBook').addEventListener('click', openBook)
+
+let checkFetch = function(response) {
+    if (!response.ok) {
+        throw Error(response + " - " + response.url)
+    }
+    return response;
+}
 
 function openBook() {
     // console.log("is this working?")
     fetch("https://randomuser.me/api/?results=5")
+    .then(checkFetch)
         .then((res) => res.json())
             .then((data) => {
                 let output = '<h2>Addresses</h2>'
@@ -30,5 +27,9 @@ function openBook() {
                     `;
                 })
                 document.getElementById("input").innerHTML = output;
+            })
+            .catch(function(err) {
+                console.log("Error")
+                console.log(err)
             })
 }
