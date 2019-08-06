6d7ca07f244019ef69f614d4971d0363726e73f8
jaywiles <jay.wiles@gmail.com>
2019-08-05 21:15:18 -0500

    part of the way there... see note on line 148

--- 06week/dodgeball/dodgeball.js
+++ 06week/dodgeball/dodgeball.js
@@ -67,8 +67,9 @@ class Person {
   }
 }
 
-class Player {
-  constructor(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience) {
+class Player extends Person{
+  constructor(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience, id, name, age, skillSet, placeBorn) {
+    super(id, name, age, skillSet, placeBorn)
     this.canThrowBall = canThrowBall;
     this.canDodgeBall = canDodgeBall;
     this.hasPaid = hasPaid;
@@ -93,6 +94,7 @@ class RedTeammate extends Player {
   }
 }
 
+// ! this function populates the list !
 const listPeopleChoices = () => {
   const listElement = document.getElementById('people')
   arrOfPeople.map(person => {
@@ -106,28 +108,18 @@ const listPeopleChoices = () => {
   })
 }
 
-// ! THIS FUNCTION IS NOT WORKING !
-// this function goes through player and person arrays and takes out any duplicates from person array since that means they were chosen to play
-const deletePerson = (id, arrOfPeople, listOfPlayers) => {
-  // looping through -- 10 is arbitrary number and can be changed to match number of people + players + blueTeam + redTeam
-  for (i = 0; i <= 10; i++) {
-    if (arrOfPeople[i].id == listOfPlayers[i].id) {
-      arrOfPeople[i].id = null;
-    }
-  }
-}
-
+// ! this function moves people from person list to player list !
 const makePlayer = (id) => {
   // console.log(`li ${id} was clicked!`);
   const player = arrOfPeople.filter (person => {
     return person.id === id;
   })[0];
 
-  const singlePlayer = new Player (arrOfPeople.filter(player => {
-    return player.id === id;
-  }))
+  // const singlePlayer = new Player (arrOfPeople.filter(player => {
+  //   return player.id === id;
+  // }))
 
-  console.log(`singlePlayer: ${singlePlayer}`);
+  // console.log(`singlePlayer: ${singlePlayer}`);
 
   listOfPlayers.push(player);
 
@@ -144,28 +136,30 @@ const makePlayer = (id) => {
     playerList.appendChild(redButton)
     // playerList.appendChild(document.createTextNode(player.name + " - " + player.skillSet))
     playerElement.append(playerList)
+    // blueButton.removeEventListener('click', function () {playerList})
     
     blueButton.innerHTML = "Choose Blue Team";
     blueButton.addEventListener('click', function () {makeBlue(player.id)})
     playerList.appendChild(blueButton)
-    playerList.appendChild(document.createTextNode("   " + player.name + " - " + player.skillSet))
+    playerList.appendChild(document.createTextNode(player.name + " - " + player.skillSet))
     playerElement.append(playerList)
+    // blueButton.removeEventListener('click', function () {makePlayer(playerList)})
+
+    // ! IF I UNCOMMENT THIS FOR LOOP - IT WILL ONLY PUT ONE PERSON DOWN TO PLAYER LIST, BUT THEN RETURNS UNDEFINED WHEN TRYING TO ADD TO TEAM, SO NOTHING HAPPENS !
+    // for (i = 0; i <= arrOfPeople.length; i++) {
+    //   if (listOfPlayers[i] == arrOfPeople[i]) {
+    //     listOfPlayers.splice(listOfPlayers.length - 1);
+    //     }
+    //   }
+    }
+    
   })
-
-  deletePerson();
-  resetMakePlayer();
 }
 
-// ! THIS FUNCTION IS NOT WORKING !
-const resetMakePlayer = () => {
-  if (listOfPlayers !== []) {
-    listOfPlayers = [];
-  }
-}
-
+// ! this function moves people from player list to red team list ... or at least it's supposed to !
 const makeRed = (id) => {
   const redPlayer = listOfPlayers.filter (person => {
-    return player.id === id;
+    return person.id === id;
   })[0];
 
   redTeam.push(redPlayer);
@@ -173,16 +167,25 @@ const makeRed = (id) => {
   console.log(redTeam);
 
   const redElement = document.getElementById('red');
-  listOfPlayers.map(player => {
+  listOfPlayers.map(person => {
     const redList = document.createElement("li");
     
+    redList.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
     redElement.append(redList)
+
+    // this for loop works to only put one person on each time... but it's always the first person who was picked to play...
+    // for (i = 0; i <= listOfPlayers.length; i++) {
+    //   if (redList[i] == listOfPlayers[i]) {
+    //     listOfPlayers.remove();
+    //   }
+    // }
   })
 }
 
+// ! this function moves people from player list to blue team list ... or at least it's supposed to !
 const makeBlue = (id) => {
   const bluePlayer = listOfPlayers.filter (person => {
-    return player.id === id;
+    return person.id === id;
   })[0];
 
   blueTeam.push(bluePlayer);
@@ -190,10 +193,18 @@ const makeBlue = (id) => {
   console.log(blueTeam);
 
   const blueElement = document.getElementById('blue');
-  listOfPlayers.map(player => {
+  listOfPlayers.map(person => {
     const blueList = document.createElement("li");
     
+    blueList.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
     blueElement.append(blueList)
+
+    // this for loop works to only put one person on each time... but it's always the first person who was picked to play...
+    // for (i = 0; i <= listOfPlayers.length; i++) {
+    //   if (blueList[i] == listOfPlayers[i]) {
+    //     listOfPlayers.remove();
+    //   }
+    // }
   })
 }
 
